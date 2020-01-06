2544b0258fcf3b9f706d296138f04c3f36d7c90c
SirPotatoIV <otoolej33@gmail.com>
2020-01-05 21:45:39 -0600

    Clickling create berder now creates a new row in the database.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,36 @@
 const createBtnEl = document.getElementById("create-btn");
 const berderBoxEl = document.getElementById("berder-box");
 
-function createHamberder(){
-    console.log(createBtnEl)
-    createBtnEl.addEventListener("click", function(){
-        // https://www.w3schools.com/jsref/event_preventdefault.asp
-        event.preventDefault()
-        const hamberderName = berderBoxEl.value;
-        console.log(hamberderName)
-    })
+async function createHamberder(){
+    await startHamberderBtn()
 }
 createHamberder();
+
+function startHamberderBtn(){
+    createBtnEl.addEventListener("click", async function(){
+        // https://www.w3schools.com/jsref/event_preventdefault.asp
+        let hamberderName = "";
+        await event.preventDefault()
+        try {
+            hamberderName = await getHamberderName();
+          } catch (error) {
+            console.error(error);
+          }
+        
+        try {
+            const response = await axios.put('/api/hamberder',{hamberderName});
+            console.log(response);
+          } catch (error) {
+            console.error(error);
+          }
+    })
+}
+
+function getHamberderName(){
+    const hamberderName = berderBoxEl.value;
+    return hamberderName;
+}
+
 // function test(){
 //     console.log("Can I get your order?!")
 // }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,8 +4,15 @@ const db = require("../models");
 // Routes
 // =====================
 module.exports = function(app){
-    app.get("api/hamberder", function(req, res){
-        console.log(req)
-        res.json("Got chu a hamberder.")
+    app.put("/api/hamberder", async function(req, res){
+        // console.log(req.body)
+        const {hamberderName} = req.body;
+        try{
+            const newHamberder = await db.Hamberders.create({hamberder_name: hamberderName})
+            res.json(newHamberder)
+        }
+        catch(err){
+            console.log("Error creating a new hamberder: ", err)
+        }
     })
 }
