b72932da11ad238e6dafe95af2076ee0e3e020fb
SirPotatoIV <otoolej33@gmail.com>
2020-01-05 20:44:58 -0600

    Create Berder button now works and gets the hamberder name in the text area.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,17 @@
-function test(){
-    console.log("Can I get your order?!")
+const createBtnEl = document.getElementById("create-btn");
+const berderBoxEl = document.getElementById("berder-box");
+
+function createHamberder(){
+    console.log(createBtnEl)
+    createBtnEl.addEventListener("click", function(){
+        // https://www.w3schools.com/jsref/event_preventdefault.asp
+        event.preventDefault()
+        const hamberderName = berderBoxEl.value;
+        console.log(hamberderName)
+    })
 }
-test();
+createHamberder();
+// function test(){
+//     console.log("Can I get your order?!")
+// }
+// test();
