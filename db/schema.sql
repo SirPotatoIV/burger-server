f3395bee5945a12cfd781d37847a43f817eae60d
SirPotatoIV <otoolej33@gmail.com>
2020-01-06 18:10:26 -0600

    Updated styling to match homework example. Updated seeds.sql and schema.sql so they actually work and mimic the sequelize models.

--- db/schema.sql
+++ db/schema.sql
@@ -6,10 +6,13 @@ CREATE DATABASE hamberders_database;
 -- Causes all following code to use the hamberders_database --
 USE hamberders_database;
 
+-- Sequelize creates the table when server.js is ran, but the homework README said to have a schema.sql so I included the table just in case. --
 -- Create the hamberders table in the hamberders_database --
 CREATE TABLE hamberders (
     id INTEGER NOT NULL AUTO_INCREMENT,
     hamberder_name VARCHAR(30) NOT NULL ,
     devoured BOOLEAN DEFAULT false NOT NULL,
+    createdAt DATETIME DEFAULT now() NOT NULL,
+    updatedAt DATETIME DEFAULT now() NOT NULL,
     PRIMARY KEY(id)
 )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,13 @@
+-- There are also seeders and migrations for sequelize-cli, but the homework said to have a seeds.sql file, so I created it just in case.
+
 USE hamberders_database;
-
 -- Creates new rows containing data in all named columns --
-INSERT INTO hamberders (hamberder_name)
-VALUES ("Black Bean");
+-- https://www.mysqltutorial.org/mysql-datetime/ --
+INSERT INTO hamberders (hamberder_name, devoured, createdAt, updatedAt)
+VALUES ("Refried Bean", false, now(), now());
 
-INSERT INTO hamberders (hamberder_name)
-VALUES ("Chili Cheese with Peanut Butter");
+INSERT INTO hamberders (hamberder_name, devoured, createdAt, updatedAt)
+VALUES ("Chili Cheese and Peanut Butter", false, now(), now());
 
-INSERT INTO hamberders (hamberder_name)
-VALUES ("Plankton and Bacon");
-
-
-
+INSERT INTO hamberders (hamberder_name, devoured, createdAt, updatedAt)
+VALUES ("Good Berder", true, now(), now());
